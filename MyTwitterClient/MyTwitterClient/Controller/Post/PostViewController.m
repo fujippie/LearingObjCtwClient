277e567c4c51ce530b10f857d6aa50c741714919
yuta_fujiwara <semanticgaps@gmail.com>
2014-07-18 12:40:22 +0900

    入力待ち画面を追加

--- MyTwitterClient/MyTwitterClient/Controller/Post/PostViewController.m
+++ MyTwitterClient/MyTwitterClient/Controller/Post/PostViewController.m
@@ -13,15 +13,9 @@
 @end
 
 @implementation PostViewController
-@synthesize delegate;
 
-#pragma mark - testDelegate
--(void) helloPostDel
-{
-    DLog("デリゲートでHelloMainを呼びます");
-    [self.delegate helloMain];
-
-}
+// TODO: 意味を調べる
+//@synthesize postDelegate;
 
 #pragma mark - LifeCycle
 
@@ -40,28 +34,64 @@
 
 #pragma mark - IBAction
 
-- (IBAction)postBtn:(id)sender
+- (IBAction)postBtn:(UIButton*)button
 {
+    [button setEnabled:NO];
+    
     NSString* str = self.postText.text;
     DLog(@"投稿内容:%@",str);
-    [self helloPostDel];
-//    画面上部の通信中エフェクト
+    
+    // 画面上部の通信中エフェクト
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
     
-    /* 
+    // 画面全体のUI操作を禁止する
+    //    self.view.userInteractionEnabled = NO;
+    
+    //処理待ち時に表示する画面
+    UIView *waitView = [[UIView alloc] initWithFrame:self.view.frame];
+    [waitView setBackgroundColor:[UIColor grayColor]];
+    waitView.alpha   = 0.7;
+    
+    //処理待ち画面中央にActivityIndicatorを表示
+    UIActivityIndicatorView *waitAi = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    [waitAi setCenter:self.view.center];
+    [waitView addSubview:waitAi];
+    [self.view addSubview:waitView];
+    [waitAi startAnimating];
+    //遅延実行　waitViewにremoveIndicatorメッセージが送られ実行される.
+    [self performSelector:@selector(_finishOperation:) withObject:@[waitView, button] afterDelay:2.0f];
+    
+    //[waitView removeFromSuperview];
+    
+    /*
      1.投稿ボタンを無効化。
      2.twitterAPIにて投稿。
      3.投稿内容を親のViewControllerへ渡し、セルを生成し、挿入。Delegate使用
-       ref. http://www.objectivec-iphone.com/introduction/delegate/delegate.html
+     ref. http://www.objectivec-iphone.com/introduction/delegate/delegate.html
      4.APIでエラーが出た場合はUIAlertをだし、投稿ボタンを有効化。
      */
+    
+    //UI操作禁止を解除する
+    //    self.view.userInteractionEnabled=YES;
     
     [self postedTweet:str];
     
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-
     
-//    [self dismissViewControllerAnimated:YES completion:nil];
+    // delegate実行のパターン
+    if (self.delegate && [self.delegate respondsToSelector:@selector(helloMain)])
+    {
+        [self.delegate helloMain];
+    }
+    
+    //    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) _finishOperation:(NSArray*)objs
+{
+    [((UIView*)objs[0]) removeFromSuperview];
+    ((UIButton*)objs[1]).enabled = YES;
+    //    self.view.userInteractionEnabled = YES;
 }
 
 - (IBAction)backBtn:(id)sender {
@@ -72,7 +102,7 @@
 #pragma mark - SubFunction
 -(void) postedTweet:(NSString*)text
 {
-
+    
     CLLocationCoordinate2D OsakaEki = CLLocationCoordinate2DMake(34.701909, 135.494977);
     TwitterAPI* twApi = [[TwitterAPI alloc] init];
     UIImage* icon = [[UIImage alloc] initWithContentsOfFile:@"femail"];
